43f3313a298fe70326261a4c22dd613f848e49c6
Gavin Jones <gjones@powerfarming.co.nz>
2018-05-31 15:17:09 +1200

    Dox

--- devrun.ps1
+++ devrun.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/pwsh
 
-# docker run --rm -it -v ${pwd}:/mnt/build -v /var/run/docker.sock:/var/run/docker.sock:ro -v bootstrap-test.sh:/home/jenkins/init/bootstrap-test.sh  gavinjonespf/docker-jenkins-slave-cake /bin/bash
+# docker run --rm -it -v ${pwd}:/mnt/build -v /var/run/docker.sock:/var/run/docker.sock:ro -v ${pwd}/bootstrap-test.sh:/home/jenkins/init/bootstrap-test.sh  gavinjonespf/docker-jenkins-slave-cake /bin/bash
 
-docker run --rm -it -v ${pwd}:/mnt/build -v /var/run/docker.sock:/var/run/docker.sock:ro -v bootstrap-test.sh:/home/jenkins/init/bootstrap-test.sh  gavinjonespf/docker-jenkins-slave-cake /scripts/init.sh
+docker run --rm -it -v ${pwd}:/mnt/build -v /var/run/docker.sock:/var/run/docker.sock:ro -v ${pwd}/bootstrap-test.sh:/home/jenkins/init/bootstrap-test.sh  gavinjonespf/docker-jenkins-slave-cake /scripts/init.sh
